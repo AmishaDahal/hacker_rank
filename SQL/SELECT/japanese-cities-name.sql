f0389b9ead01cc47e66ae3d2d3415aadbaf9fdb6
AmishaDahal <68108618+AmishaDahal@users.noreply.github.com>
2026-01-15 08:23:49 -0700

    Create japanese-cities-name.sql

---
+++ SQL/SELECT/japanese-cities-name.sql
@@ -0,0 +1,19 @@
+/*
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+Given a City table, whose fields are described as
+
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | Number   |
+| Name        | Char(35) |
+| CountryCode | Char(3)  |
+| District    | Char(20) |
+| Population  | Number   |
++-------------+----------+
+
+you have to print all the details of all the cities of Japan. The CountryCode for Japan is "JPN".
+*/
+select Name
+from City
+where COUNTRYCODE ='JPN';
