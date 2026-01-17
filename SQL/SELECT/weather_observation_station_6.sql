9dd8cb27c593b328ee01b7075ae4d45c484a819c
AmishaDahal <68108618+AmishaDahal@users.noreply.github.com>
2026-01-16 20:32:13 -0700

    Create weather_observation_station_6.sql

---
+++ SQL/SELECT/weather_observation_station_6.sql
@@ -0,0 +1,20 @@
+/*
+Weather Observation Station 6
+=============================
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+Write a query to print the list of CITY that start with vowels in lexicographical order. Do not print duplicates.
+*/
+select distinct City
+from Station
+where substring(City,1,1) in ('a','e','i','o','u');
