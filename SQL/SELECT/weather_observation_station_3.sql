e12e6b286977f57fe5fae4f433a38fb99412c696
AmishaDahal <68108618+AmishaDahal@users.noreply.github.com>
2026-01-16 20:24:33 -0700

    Create weather_observation_station_3.sql

---
+++ SQL/SELECT/weather_observation_station_3.sql
@@ -0,0 +1,21 @@
+/*
+Weather Observation Station 3
+
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+Write a query to print the list of CITY in lexicographical order for even ID only. Do not print duplicates.
+*/
+
+select distinct city
+from station 
+where Mod(ID,2)=0;
