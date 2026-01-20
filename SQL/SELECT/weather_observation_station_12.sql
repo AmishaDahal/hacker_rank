92eb1bbe9bb2fbf8cb9877b2379e94f655831e78
AmishaDahal <68108618+AmishaDahal@users.noreply.github.com>
2026-01-19 22:38:46 -0700

    Create weather_observation_station_12.sql

---
+++ SQL/SELECT/weather_observation_station_12.sql
@@ -0,0 +1,20 @@
+/*
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+Write a query to print the list of CITY that does not start with vowels and does not end with vowels in lexicographical order. Do not print duplicates.
+*/
+
+select distinct city
+from station 
+where left(city,1) not in ('a','e','i','o','u')
+and right(city,1) not in ('a','e','i','o','u');
